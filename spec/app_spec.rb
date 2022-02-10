f42c3e90814deb6c1d8539177c035df6c05c4d46
irizaga <gibran.irizaga@gmail.com>
2022-02-10 11:15:53 -0300

    fixed css and tests added

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,6 +3,7 @@
 require_relative '../app'
 require 'sinatra/base'
 require 'rack/test'
+require_relative 'spec_helper'
 
 RSpec.configure do |conf|
   conf.include Rack::Test::Methods
@@ -11,31 +12,38 @@ end
 describe 'App' do
   let(:app) { Sinatra::Application }
 
-  context 'request / ' do
+  describe 'request / ' do
     let(:response) { get '/' }
     it 'returns 200' do
       expect(response.status).to eq 200
     end
   end
 
-  context 'request /personal ' do
+  describe 'request /personal ' do
     let(:response) { get '/personal' }
     it 'returns 200' do
       expect(response.status).to eq 200
     end
   end
 
-  context 'request /pull_requests ' do
+  describe 'request /pull_requests ' do
     let(:response) { get '/pullrequests' }
     it 'returns 200' do
       expect(response.status).to eq 200
     end
   end
 
-  context 'request /academic ' do
+  describe 'request /academic ' do
     let(:response) { get '/academic' }
     it 'returns 200' do
       expect(response.status).to eq 200
+    end
+  end
+
+  describe 'request /invalid ' do
+    let(:response) { get '/invalid' }
+    it 'returns 404' do
+      expect(response.status).to eq 404
     end
   end
 end
